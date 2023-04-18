ad1c0be1a7a61a24f37139b0a55b80888e2c49a8
Revyrin <131164659+Revyrin@users.noreply.github.com>
2023-04-19 06:44:35 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N - 1; i++) {
+		for (int k = 1; k < i; k++)
+			cout << ' ';
+		for (int k = 1; k <= (N - i) * 2 + 1; k++)
+			cout << '*';
+		cout << "\n";
+	}
+
+	for (int i = 1; i <= N - 1; i++)	cout << ' ';
+	cout << "*\n";
+
+	for (int i = N - 1; i >= 1; i--) {
+		for (int k = 1; k < i; k++)
+			cout << ' ';
+		for (int k = 1; k <= (N - i) * 2 + 1; k++)
+			cout << '*';
+		cout << "\n";
+	}
+}

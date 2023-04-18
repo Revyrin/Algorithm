6d199cf2401565dd2b7692a349b70b6c6cba267b
Revyrin <131164659+Revyrin@users.noreply.github.com>
2023-04-19 03:59:47 +0900

    [Bronze II] Title: 숫자, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	long long A, B;
+	cin >> A >> B;
+	if (A > B) swap(A, B);
+
+	if (B != A) cout << B - A - 1 << "\n";
+	else cout << 0 << "\n";
+
+	for (long long i = A + 1; i < B; i++)
+		cout << i << " ";
+	
+}

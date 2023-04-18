1443260e330da77f6f22232b4ecf046d0dfa3ded
Revyrin <131164659+Revyrin@users.noreply.github.com>
2023-04-19 01:58:25 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	vector<int> v(5);
+	for (int i = 0; i < 5; i++)	cin >> v[i];
+
+	sort(v.begin(), v.end());
+	cout << accumulate(v.begin(), v.end(), 0) / v.size() << "\n" << v[2];
+}

35d33430594ac077297dd6782abc7edd627b6ffc
Revyrin <131164659+Revyrin@users.noreply.github.com>
2023-04-30 03:38:12 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,40 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 배열로 스택 구현
+int st[10001];
+int idx;
+
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int N;
+	int N, X;
 	cin >> N;
-
-	stack<int> st;
 	string in;
-	int X;
 
 	while (N--) {
 		cin >> in;
 		if (in == "push") {
 			cin >> X;
-			st.push(X);
+			st[idx++] = X;
 		}
 		else if (in == "pop") {
-			if (!st.empty()) {
-				cout << st.top() << '\n';
-				st.pop();
+			if (idx == 0)
+				cout << -1 << '\n';
+			else {
+				cout << st[--idx] << '\n';
 			}
-			else cout << -1 << '\n';
 		}
 		else if (in == "size") {
-			cout << st.size() << '\n';
+			cout << idx << '\n';
 		}
 		else if (in == "empty") {
-			if (st.empty()) cout << 1 << '\n';
+			if (idx == 0) cout << 1 << '\n';
 			else cout << 0 << '\n';
 		}
 		else if (in == "top") {
-			if (!st.empty()) cout << st.top() << '\n';
-			else cout << -1 << '\n';
+			if (idx == 0) cout << -1 << '\n';
+			else cout << st[idx - 1] << '\n';
 		}
 	}
 }

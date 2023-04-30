f198624495579f412a0622116e618a0fc6e367eb
Revyrin <131164659+Revyrin@users.noreply.github.com>
2023-05-01 03:09:39 +0900

    [Silver IV] Title: 큐 2, Time: 392 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	queue<int> q;
+
+	while (n--) {
+		string in;
+		int x;
+		cin >> in;
+		if (in.compare("push") == 0) {
+			cin >> x;
+			q.push(x);
+		}
+		else if (in.compare("pop") == 0) {
+			if (q.empty()) cout << -1 << '\n';
+			else {
+				cout << q.front() << '\n';
+				q.pop();
+			}
+		}
+		else if (in.compare("size") == 0) {
+			cout << q.size() << '\n';
+		}
+		else if (in.compare("empty") == 0) {
+			if (q.empty()) cout << 1 << '\n';
+			else cout << 0 << '\n';
+		}
+		else if (in.compare("front") == 0) {
+			if (q.empty()) cout << -1 << '\n';
+			else cout << q.front() << '\n';
+		}
+		else if (in.compare("back") == 0) {
+			if (q.empty()) cout << -1 << '\n';
+			else cout << q.back() << '\n';
+		}
+	}
+}
